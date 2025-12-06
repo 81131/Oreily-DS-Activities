3968a5a4710fa0c5a26b5b1ed71105a83d84bddb
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-06 15:33:21 +0530

    Added dictionary indexing and creation

---
+++ Crash_Course_in_Python/Data Structures/Dictionaries/BasicDict.py
@@ -0,0 +1,10 @@
+#Creating empty dictionaries
+emptyDict1 = {}
+emptyDict2 = dict()
+
+#Dictionary with values
+dict1 = {"Name": "Dinindu", "Age": 21}
+
+#Aceesing a value
+print(f"Name: {dict1["Name"]}")
+
