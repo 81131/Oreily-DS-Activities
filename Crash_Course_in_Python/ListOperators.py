a181c1a022d36b8acc4032e65ed28f6514075c12
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-06 09:46:23 +0530

    Added ListOperators.py

---
+++ Crash_Course_in_Python/ListOperators.py
@@ -0,0 +1,5 @@
+# In operator (Use to check availability of an element)
+list1 = [59,8,46,15,18,78,236,87]
+print(f"Original List: {list1}")
+print(f"Check if 18 available in list1 (18 in list1): {'Yes' if 18 in list1 else 'No'}")
+
