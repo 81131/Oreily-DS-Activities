87009817cafd3099eeb19fb287407fdcc9cb1fb6
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-06 10:43:15 +0530

    Updated file structure

--- Crash_Course_in_Python/Lists/ListOperators.py
+++ Crash_Course_in_Python/Lists/ListOperators.py
@@ -26,3 +26,10 @@ print(f"Original List: {list1}")
 list1.append(92)
 print(f"list1.append(92)")
 print(f"List after appending: {list1}")
+
+
+#Unpacking a list
+print(f"\n\nx, y = [100,500]")
+x, y = [100,500]
+print(f"x = {x}\ny= {y}")
+
