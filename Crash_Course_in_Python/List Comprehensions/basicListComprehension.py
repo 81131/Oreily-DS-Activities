e770387d81ff6312e68b2493675666cf57961fb0
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-21 18:33:21 +0530

    Added basic list comphrehension use cases

---
+++ Crash_Course_in_Python/List Comprehensions/basicListComprehension.py
@@ -0,0 +1,7 @@
+#Comprehensation basically means deriving some value from one another
+#Here we are deriving square of the even numbers using original even numbers
+evenNumbers = [x for x in range[10] if x % 2 == 0]
+evenSquares = [x * x for x in evenNumbers]
+
+print("Even numbers: ", *evenNumbers)
+print("Even Squares: ", *evenSquares)
