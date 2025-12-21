b70179cb2f5382e4cd04ebb283fd117eb12ac5e7
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-21 19:24:34 +0530

    Added basic assertion expression with warning messages.

---
+++ Crash_Course_in_Python/Automated Testing and Assert/basicAssertExample.py
@@ -0,0 +1,11 @@
+#Basically Assertion error are developer made rules which will throw an error message if broken. They are used to 
+#For an example lets say we want to check the functionality of a min - max function. 
+
+def MaxFinder (num1: int, num2: int):
+    if num1 < num2:
+        return num1
+    else:
+        return num2
+
+#Clrarly the function iswrong, but yet it execute. So we will add kind of a test subject to verify the functionality. 
+assert MaxFinder(10,20) == 20, "Test with 10 vs 20 -> The 20 is larger but function did not get it correct"
