0935e051d1c1447360a3c291183f0ad717f8b667
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-22 22:05:31 +0530

    Added random range example

---
+++ Crash_Course_in_Python/Randomness/randomRaange.py
@@ -0,0 +1,7 @@
+#Random range is used to generate a randomnumber between a given range
+
+import random
+
+num1 = random.randrange(8,15)
+print(num1)
+#This will genearte a number between 8(Inclusive) & 15(Exclusive). As seed is not set every time the script run, it will generate a different number
