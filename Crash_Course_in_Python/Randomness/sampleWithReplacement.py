70df337235e9f3ad2ded2e989694d6c7f5d0c6a1
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-24 18:30:29 +0530

    Added samples with replacement examples

---
+++ Crash_Course_in_Python/Randomness/sampleWithReplacement.py
@@ -0,0 +1,41 @@
+#This look just same as sample without replacement, but instead of sample we use choice. Same number can appear multiple times now.
+
+import random
+
+#Low Number Range = More likely to win
+numRange = range(10)
+positions = 6
+lotteryNumbers = [random.choice(numRange) for _ in range(positions)]
+winningNumbers = [random.choice(numRange) for _ in range(positions)]
+
+print("Your lottery numbers are: ", *lotteryNumbers)
+print("Winning numbers are: ", *winningNumbers)
+
+#Congrats if you get at least 100 :) It's almost like a real life lottery 
+def checkWinner(lotteryNumbers: list, winningNumbers: list): 
+    winCount = 0
+    moneyPool = 0
+    for i,x in zip(lotteryNumbers, winningNumbers):
+        if i == x:
+            winCount +=1
+
+    match winCount:
+        case 6:
+            moneyPool = 800000
+        case 5: 
+            moneyPool = 400000
+        case 4:
+            moneyPool = 20000
+        case 3:
+            moneyPool = 1000
+        case 2:
+            moneyPool = 500
+        case 1:
+            moneyPool = 100
+        case 1:
+            pass
+    
+    return winCount, moneyPool
+
+winCount, moneyPool = checkWinner(lotteryNumbers, winningNumbers)
+print(f"{winCount} numbers matched and you won ${moneyPool}!")
