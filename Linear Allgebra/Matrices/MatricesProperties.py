96f341e1e2aa5a8ec1e5ca01b2354e0a123f50fb
81131 <saluwadanagedaradinindu@gmail.com>
2025-12-30 19:14:26 +0530

    Added get_row, get_column & shape methods for matrices

---
+++ Linear Allgebra/Matrices/MatricesProperties.py
@@ -0,0 +1,41 @@
+from typing import List
+from typing import Tuple
+
+Vector = List[float]
+Matrix = List[List[float]]
+
+A = [[1,2,3],
+     [4,5,6]]
+#A has 2 rows + 3 columns -> Shape = (2,3)
+
+
+
+
+B = [[1,2],
+     [3,4],
+     [5,6]]
+#B has 3 rows + 2 columns -> Shape = (3,2)
+
+
+def shape(Matrix1:Matrix) -> Tuple[int, int]:
+    """Returns (# of rows of A, # of columns of B)"""
+
+    #Get the row count
+    numOfRows = len(Matrix1)
+    numOfCols = len(Matrix1[0]) if Matrix1 else 0
+    return numOfRows, numOfCols
+
+assert shape(A) == (2,3)
+
+
+
+def get_row(Matrix1:Matrix, i:int) -> Vector:
+    """Returns the ith row of the provided matrix"""
+    return Matrix1[i]
+assert(get_row(A, 0)) == [1,2,3]
+
+
+def get_column(Matrix1: Matrix, j:int) -> Vector:
+    """Returns the jth column of a matrix as a vector"""
+    return [Matrix1_i[j] for Matrix1_i in Matrix1]
+assert (get_column(A, 1)) == [2,5]
