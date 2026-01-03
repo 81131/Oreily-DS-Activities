762e623088ca3638c659c796672e5676ac271c27
81131 <saluwadanagedaradinindu@gmail.com>
2026-01-03 18:18:57 +0530

    Added standard deviation function

--- Statistics/Describe a Single Set of Data/Dispersion.py
+++ Statistics/Describe a Single Set of Data/Dispersion.py
@@ -2,6 +2,7 @@
 
 from collections import Counter
 from typing import List
+import math
 num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
 
@@ -31,9 +32,17 @@ def _mean_dif(dataset: List[float]) -> float:
 
 #Helper function to get sum of squares
 def sum_of_squares(inputList: List[float]) -> float:
+    """Returns the sum of squares of for a given list"""
     return sum(x**2 for x in inputList)
 
 def variance(dataset: List) -> float:
+    """Returns the variance for a given list"""
     deviations = _mean_dif(dataset)
     n = len(dataset)
     return sum_of_squares(deviations) / (n-1)
+
+
+#3. Standard Deviation (This is the square root of the variance)
+def standard_deviation(dataset: List) -> float: 
+    """Returns the standard deviation for a given list"""
+    return math.sqrt(variance(dataset))
