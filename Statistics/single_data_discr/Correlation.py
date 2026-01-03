eac85c295174db7468e356d21453230cc6d70aff
81131 <saluwadanagedaradinindu@gmail.com>
2026-01-03 21:26:55 +0530

    Added covariation function

--- Statistics/single_data_discr/Correlation.py
+++ Statistics/single_data_discr/Correlation.py
@@ -4,6 +4,7 @@ from Statistics.single_data_discr.CentralTendencies import mean
 from Statistics.single_data_discr.Dispersion import _mean_dif
 
 def covariance (dataset1: List[float], dataset2: List[float]) -> float:
+    """Returns the covariance of the dataset1 & dataset2"""
     assert len(dataset1) == len(dataset2), "Both dataset lengths should be same"
-
     return dot(_mean_dif(dataset1), _mean_dif(dataset2)) / (len(dataset1) - 1)
+
