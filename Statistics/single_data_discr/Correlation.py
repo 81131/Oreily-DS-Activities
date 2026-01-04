f0bc94f177b9f66f81f049f359d54106a962b2dd
81131 <saluwadanagedaradinindu@gmail.com>
2026-01-04 20:15:57 +0530

    Added correlation function

--- Statistics/single_data_discr/Correlation.py
+++ Statistics/single_data_discr/Correlation.py
@@ -1,7 +1,7 @@
 from typing import List
 from Linear_Algebra.Vectors.vectorProperties import dot
 from Statistics.single_data_discr.CentralTendencies import mean
-from Statistics.single_data_discr.Dispersion import _mean_dif
+from Statistics.single_data_discr.Dispersion import _mean_dif, standard_deviation
 
 
 #1. Covariance
@@ -11,3 +11,13 @@ def covariance (dataset1: List[float], dataset2: List[float]) -> float:
     assert len(dataset1) == len(dataset2), "Both dataset lengths should be same"
     return dot(_mean_dif(dataset1), _mean_dif(dataset2)) / (len(dataset1) - 1)
 
+#2. Correlation 
+#Formula: r = Cov(X, Y) / (σₓσᵧ)
+def correlation(dataset1: List[float], dataset2: List[float]) -> float:
+    """Measures how much dataset1 and dataset ]2 vary in tandem about their means"""
+    stdev_dataset1 = standard_deviation(dataset1)
+    stdev_dataset2 = standard_deviation(dataset2)
+    if stdev_dataset1> 0 and stdev_dataset2 > 0:
+        return covariance(dataset1, dataset2) / stdev_dataset1/ stdev_dataset2
+    else:
+        return 0
